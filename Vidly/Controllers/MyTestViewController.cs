cdcb10e23f5cac7708182fccb7e2e57872a0c660
mzare747 <masoudzare747@gmail.com>
2016-06-28 16:55:45 +0330

    MyTestView_13950408_1655

---
+++ Vidly/Controllers/MyTestViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vidly.Controllers
+{
+    public class MyTestViewController : Controller
+    {
+        // GET: MyTestView
+        public ActionResult MyTestView()
+        {
+            return View();
+        }
+    }
+}
